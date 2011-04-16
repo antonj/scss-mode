013f202bb466f51b9f03b40692f696c57e2b6fe0
jgv <jvingiano@gmail.com>
2011-04-16 18:13:59 -0400

    modifying variable regex to account for digits in variables

--- scss-mode.el
+++ scss-mode.el
@@ -56,7 +56,7 @@ HYPERLINK HIGHLIGHT)"
 
 (defconst scss-font-lock-keywords
   ;; Variables
-  '(("\$[a-z_-]+" . font-lock-constant-face)
+  '(("\$[a-z_-0-9]+" . font-lock-constant-face)
     ("//.*$" . font-lock-comment-face)))
 
 (defun scss-compile-maybe()
