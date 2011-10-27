e4321b5d775dd922f446f2e6c74975f9cee5a3ec
Anderson De Andrade <adeandradeds@gmail.com>
2011-10-27 10:59:11 -0430

    Support a custom output directory for compiled files. Added scss-directory-output as a customized variable.

--- scss-mode.el
+++ scss-mode.el
@@ -53,6 +53,11 @@
 '(\"--cache-location\" \"'/tmp/.sass-cache'\")"
   :group 'scss)
 
+(defcustom scss-output-directory nil
+  "Output directory for compiled files, for example:
+\"../css\""
+  :group 'scss)
+
 (defcustom scss-compile-error-regex '("\\(Syntax error:\s*.*\\)\n\s*on line\s*\\([0-9]+\\) of \\([^, \n]+\\)" 3 2 nil nil 1)
   "Regex for finding line number file and error message in
 compilation buffers, syntax from
@@ -74,7 +79,10 @@ HYPERLINK HIGHLIGHT)"
   (interactive)
   (compile (concat scss-sass-command " " (mapconcat 'identity scss-sass-options " ") " "
                    "'" buffer-file-name "' '"
-                   (first (split-string buffer-file-name "[.]scss$")) ".css'")))
+                   (if scss-output-directory
+                       (when (string-match ".*/\\(.+\\)[.]scss$" buffer-file-name)
+                         (concat scss-output-directory "/" (match-string 1 buffer-file-name)))
+                     (first (split-string buffer-file-name "[.]scss$"))) ".css'")))
 
 ;;;###autoload
 (define-derived-mode scss-mode css-mode "SCSS"
